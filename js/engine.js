3d30aed562185785b0411c6d5d2217d3d934b8c0
Kirk Dietz <kdietz1@email.davenport.edu>
2014-11-02 13:43:02 -0500

    Update
    Changed clear color

--- js/engine.js
+++ js/engine.js
@@ -25,7 +25,7 @@ function start() {
   // Only continue if WebGL is available and working
   
   if (gl) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
+    gl.clearColor(1.0, 1.0, 1.0, 1.0); 
     gl.clearDepth(1.0);                 // Clear everything
     gl.enable(gl.DEPTH_TEST);           // Enable depth testing
     gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
